526b439599cf35753f509ad2a67529f44f96a201
jff07r <jfernandezf@alfatecsistemas.es>
2021-09-11 09:23:43 +0200

    Añadido ciudad y edad

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -3,8 +3,13 @@ package ejemplo;
 public class Ejemplo {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		System.out.println("----------------------");
 		String nombre = "Jose";
 		String apellido = "Fernández";
-		System.out.println(nombre + apellido);
+		System.out.println("Usuario: " + nombre + apellido);
+		String ciudad = "Murcia";
+		System.out.println("Ciudad: " + ciudad);
+		int edad = 30;
+		System.out.println("Edad: " + edad);		
 	}
 }
